cf024b0730b5bb7ca3086122e41f9746e4a44659
MuthuRanjan <muthuranjan05@gmail.com>
2026-09-06 12:21:37 +0530

    Sync frontend updates with assistant bot and login fixes

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 
 import api from "../api/axiosInstance";
